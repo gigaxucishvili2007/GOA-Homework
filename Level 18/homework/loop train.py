b6acbd90e97fabe24ad77e62aa199191344f27ea
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2024-08-03 09:34:44 +0400

    commit

---
+++ Level 18/homework/loop train.py
@@ -0,0 +1,82 @@
+# 2) ივარჯიშეთ განვლილ მასალაზე, კონკრეტულად Loop - ზე, for
+#  loop
+#  & while loop - ზე სათითაოდ გააკეთეთ 10 მაგალითი.
+# while loop example 10:
+while 5 > 3:
+    print("სწორი ხარ 5 მეტია 3 ზე")
+
+while 4 - 1:
+    print("Correct")
+
+while 7 % 4:
+    print("success")
+
+while 8 <= 3:
+    print("hello happy birthday")
+
+while 3 * 1:
+    print("მათემატიკური შედეგი")
+
+while 10 / 5:
+    print("GOA IS THE BEST")
+
+while 15 // 9:
+    print("bravo")
+
+while 17 + 5:
+    print("GOA")
+
+while 20 % 5:
+    print("ნაშთი 0")
+
+while 25 ** 15:
+    print("დავალება წარმატებით დასრულე")
+
+# for loop example 10:
+for i in range(0,16,1):
+    print(i)
+#for loop's 1 example complete:
+
+for i in range(0,21,1):
+    print(i, "GOA BEST")
+#for loop's 2 example complete: 
+
+for i in range(2,5,1):
+    print(i, "for loop example")
+    # for loop's 3 example complete:
+
+for i in range(0,9,1):
+    print(i, "numbers")
+    #for loop's 4 example complete:             
+
+for i in range(0,501,10):
+    print("Terminal")
+    # for loop's 5 example complete:
+# პირველი ციფრი დაიწყებს იქიდან ათვლას რა რიცხვსაც მიუთითებთ
+#მეორე ციფრი გაგრძელდება იქამდე რა რიცხვსაც მივუთითებთ
+#ხოლო მეოთხე ციფრი ათვლას დაიწყებს 7იდან ანუ ყოველ 5 ს ყოველი 7 მოემატება სანამ არ ავა 25 მდე
+fruits = ["banana","orange", "Kiwi"]
+for x in fruits:
+    print(x)
+    # for loop's 6 example complete:
+
+cars = ["Mercedes", "BMW", "astonmartin"]
+for x in cars:
+    print(cars)
+    # for loop's 7 example complete:
+
+for x in "food":
+    print(x)
+    # for loop's 8 example complete:
+for x in range(6):
+    print(x)
+    # for loop's 9 example complete:
+
+for i in range(6):
+    print(i)
+    # for loop's 10 example complete:
+
+for x in range(2,6):
+    print(x)
+    # for loop's 10 complete complete:
+
