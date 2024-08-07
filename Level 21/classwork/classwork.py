365a2668496fd4ee94dda8976dd0d213b723ba3d
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2024-08-07 13:11:14 +0400

    commit

---
+++ Level 21/classwork/classwork.py
@@ -0,0 +1,28 @@
+num = 30
+while num <= 70:
+    print(num)
+    num +=1
+
+for i in range(100):
+    print("GOA BEST")
+
+რიცხვი = 10
+while რიცხვი < 5:
+    print("My name")
+
+for i in range(0,26,5):
+    print(i, "Hello world")
+
+number = 50
+while number > 45:
+    print("loop")
+
+if 5 > 4:
+    print("Mr levani")
+else:
+    print("nothing")
+
+num = 300
+while num <= 400:
+        print(num)
+        num +=1
