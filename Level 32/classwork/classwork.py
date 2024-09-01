5d5a1bae500d6bdbf57c41fbc76b193d163bf0cc
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2024-09-01 09:37:32 +0400

    commit

---
+++ Level 32/classwork/classwork.py
@@ -0,0 +1,95 @@
+# შექმენით ფუნქცია, რომელიც გამოიტანს x და y ჯამს, მოახდინეთ ოპერაცია შემდეგ ოპერატორებზე: +, -. *. //
+# x = 12
+# y = 8
+# def sum():
+    # print(x + y)
+    # print(x * y)
+    # print(y - x)
+    # print(x / y)
+    # print(y // x)
+# sum()
+
+# შექმენით ფუნქცია, რომელიც მიესალმება მომხმარებელს.
+# ტექსტი: 'გამარჯობა {სახელი}, კეთილი იყოს შენი მობრძანება,  გისურვებ წარმატებას და წინ სვლას"
+# def hello():
+    # print("hello {David},Welcome, I wish you success and progress")
+# hello()
+
+# შექმენით ფუნქცია, რომელიც სიყვარულის წერილს მიუძღვნის მომხმარებელს:
+# ტექსტი თქვენ მოიფიქრეთ.
+
+
+# def love_letter():
+#     print("Hello, my dear customer, I have a discount for you. I love you very much - 55% discount on anything you want to buy, because we also have gift cards. affention {giga}")
+# love_letter()
+# ტექსტის ბოლოს აუცილებლად ჩაამატეთ ეს ნაწილი: 'სიყვარულით {სახელი}'
+
+# შექმენით ფუნქცია, სადაც შემოიტანთ თქვენს თავზე ინფორმაცია, სახელი, გვარი, ასაკი, ქვეყანა, ქალაქი, საყვარელი ჰობი და ა.შ
+# შემდე ფუნქციის დახმარებით ააგეთ ერთი დიდი წინადადება. 
+
+# def Information_about_myself():
+#     print('Hello, I am Giga Khutsishvili, 16 years old, I love living, I was born in November, November 11, 2007. My hobby is programming, I have trouble with small issues, but I can overcome everything.')
+# Information_about_myself()
+
+
+# გამოთვალეთ მართკუთხედის ფართობი
+
+# შექმენით ფუქნქცია, რომელსაც ეწოდება calculate_area
+
+# ამ ფუნქციაში უნდა იყვეს ორი პარამეტრი, length და width
+
+# გამოიყენეთ ფორმულა, area = length * width
+# length = 150
+# width = 821
+# area = 123
+# def calculate_area():
+#     print(area * length * width)
+# calculate_area()
+
+# მაქსიმალური რიცხვის პოვნა:
+
+# განსაზღვრეთ ფუნქცია სახელით find_max.
+
+# ფუნქციამ უნდა მიიღოს ორი პარამეტრი: num1 და num2 (ორივე მთელი ან წილადი რიცხვი).
+
+# 3.ფუნქციამ უნდა შეადაროს ორი რიცხვი და დააბრუნოს მათი მაქსიმალური მნიშვნელობა.
+
+# def ები ახალი ახსნილიგვაქვს და ამას გავაკეთებ მერე.
+num1 = 120
+
+num2 = 111
+def find_max():
+    if num1 == num2:
+        return(num1 ** num2)
+
+find_max()
+
+# შექმენით ფუქნცია და შეამოწმეთ რიცხვი ლუწია თუ კენტი.
+
+num1 = 2
+
+num2 = 4
+
+num3 = 3
+
+num4 = 7
+
+def check_number():
+    
+    if num1 % 2:
+        
+        print("this is even")
+    
+    elif num2 == 4:
+        
+        print("This number is also even")
+    
+    elif num3 == 3:
+        
+        print("this number is Odd")
+    
+    elif num4 > 4:
+        
+        print("This is also odd")
+check_number()
+
