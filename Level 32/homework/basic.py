65bc2162c1be2ba4dc7f5f318ab417c7cf9151e7
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2024-09-01 08:56:13 +0400

    commit

---
+++ Level 32/homework/basic.py
@@ -0,0 +1,74 @@
+
+
+
+num1 = 15
+
+num2 = 12
+
+operation = 'add'
+
+def simple_calculator():
+    print(num1 + num2)
+
+
+simple_calculator()
+
+if operation == 'add':
+    
+    print("Correct you can easily divide 15 by 12")
+
+else:
+    print("Error: Cannot divide by zero")
+
+    
+def arithmethic_operator():
+        print(10 + 2)
+        print(10 - 1)
+        print(10 - 3)
+        print(10 + 7)
+arithmethic_operator()
+
+
+bmi_weight = 100
+
+def bmi_calculator():
+    
+    print(bmi_calculator)
+
+if bmi_weight < 100:
+    print("above average")
+
+elif bmi_weight == 100:
+    
+    
+    print("very big")
+
+elif bmi_weight == 45:
+    
+    print("normal")
+
+elif bmi_weight == 65:
+    
+    print("medium")
+else:
+    print("healthy weight")  
+
+
+# Level 32:
+ 
+# ==
+
+# განსაზღვრეთ ფუნქცია სახელწოდებით simple_calculator, რომელიც იღებს სამ არგუმენტს:
+
+# num1: პირველი რიცხვი (მთლიანი ან float).
+# num2: მეორე რიცხვი (მთლიანი ან float).
+
+# ოპერაცია: სტრიქონი, რომელიც განსაზღვრავს შესასრულებლად ოპერაციას. ის შეიძლება იყოს „დამატება“, „გამოკლება“, „გამრავლება“ ან „გაყოფა“.
+# ფუნქციის შიგნით,
+
+# გამოიყენეთ if და elif განცხადებები, რათა დაადგინოთ რომელი ოპერაცია უნდა შეასრულოთ ოპერაციის არგუმენტის მნიშვნელობიდან გამომდინარე.
+
+# ფუნქციამ უნდა დააბრუნოს ოპერაციის შედეგი.
+
+# თუ ოპერაცია არის „გაყოფა“ და num2 არის 0, ფუნქციამ უნდა დააბრუნოს „შეცდომა: ნულზე გაყოფა შეუძლებელია“.
+# ჩაწერეთ კოდი, რომ გამოიძახოთ ფუნქცია სხვადასხვა ოპერაციებით და დაბეჭდოთ შედეგები
