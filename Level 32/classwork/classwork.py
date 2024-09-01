bc3f6d304f6979130e301a3989e2ae74d55aafa3
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2024-09-01 09:39:43 +0400

    commit

--- Level 32/classwork/classwork.py
+++ Level 32/classwork/classwork.py
@@ -1,35 +1,35 @@
 # შექმენით ფუნქცია, რომელიც გამოიტანს x და y ჯამს, მოახდინეთ ოპერაცია შემდეგ ოპერატორებზე: +, -. *. //
-# x = 12
-# y = 8
-# def sum():
-    # print(x + y)
-    # print(x * y)
-    # print(y - x)
-    # print(x / y)
-    # print(y // x)
-# sum()
+x = 12
+y = 8
+def sum():
+    print(x + y)
+    print(x * y)
+    print(y - x)
+    print(x / y)
+    print(y // x)
+sum()
 
 # შექმენით ფუნქცია, რომელიც მიესალმება მომხმარებელს.
 # ტექსტი: 'გამარჯობა {სახელი}, კეთილი იყოს შენი მობრძანება,  გისურვებ წარმატებას და წინ სვლას"
-# def hello():
-    # print("hello {David},Welcome, I wish you success and progress")
-# hello()
+def hello():
+    print("hello {David},Welcome, I wish you success and progress")
+hello()
 
 # შექმენით ფუნქცია, რომელიც სიყვარულის წერილს მიუძღვნის მომხმარებელს:
 # ტექსტი თქვენ მოიფიქრეთ.
 
 
-# def love_letter():
-#     print("Hello, my dear customer, I have a discount for you. I love you very much - 55% discount on anything you want to buy, because we also have gift cards. affention {giga}")
-# love_letter()
+def love_letter():
+    print("Hello, my dear customer, I have a discount for you. I love you very much - 55% discount on anything you want to buy, because we also have gift cards. affention {giga}")
+love_letter()
 # ტექსტის ბოლოს აუცილებლად ჩაამატეთ ეს ნაწილი: 'სიყვარულით {სახელი}'
 
 # შექმენით ფუნქცია, სადაც შემოიტანთ თქვენს თავზე ინფორმაცია, სახელი, გვარი, ასაკი, ქვეყანა, ქალაქი, საყვარელი ჰობი და ა.შ
 # შემდე ფუნქციის დახმარებით ააგეთ ერთი დიდი წინადადება. 
 
-# def Information_about_myself():
-#     print('Hello, I am Giga Khutsishvili, 16 years old, I love living, I was born in November, November 11, 2007. My hobby is programming, I have trouble with small issues, but I can overcome everything.')
-# Information_about_myself()
+def Information_about_myself():
+    print('Hello, I am Giga Khutsishvili, 16 years old, I love living, I was born in November, November 11, 2007. My hobby is programming, I have trouble with small issues, but I can overcome everything.')
+Information_about_myself()
 
 
 # გამოთვალეთ მართკუთხედის ფართობი
@@ -39,12 +39,12 @@
 # ამ ფუნქციაში უნდა იყვეს ორი პარამეტრი, length და width
 
 # გამოიყენეთ ფორმულა, area = length * width
-# length = 150
-# width = 821
-# area = 123
-# def calculate_area():
-#     print(area * length * width)
-# calculate_area()
+length = 150
+width = 821
+area = 123
+def calculate_area():
+    print(area * length * width)
+calculate_area()
 
 # მაქსიმალური რიცხვის პოვნა:
 
